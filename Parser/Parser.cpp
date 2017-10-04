b30b3c7c5f8a620ba79908a826da556d9f460f9f
gajrajkuldeep <gajrajkuldeep@gmail.com>
2017-10-04 13:12:08 -0700

    Add project files.

---
+++ Parser/Parser.cpp
@@ -0,0 +1,13 @@
+// Parser.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+
+	printf("changed it later why later we will do it now");
+    return 0;
+}
+
